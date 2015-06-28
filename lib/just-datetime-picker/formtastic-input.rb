432a8a87d45e41f20a04c158152cf163bc26fdc0
bkubic <ben.kubic@gmail.com>
2015-06-28 16:26:10 -0400

    Update formtastic-input.rb

--- lib/just-datetime-picker/formtastic-input.rb
+++ lib/just-datetime-picker/formtastic-input.rb
@@ -33,7 +33,9 @@ module Formtastic
 
           builder.text_field("#{method}_time_hour", input_html_options.merge({ :class => "just-datetime-picker-field just-datetime-picker-time just-datetime-picker-time-hour", :value => hour_value, :maxlength => 2, :size => 2 })) <<
           ":" <<
-          builder.text_field("#{method}_time_minute", input_html_options.merge({ :class => "just-datetime-picker-field just-datetime-picker-time just-datetime-picker-time-minute", :value => minute_value, :maxlength => 2, :size => 2 }))
+          builder.text_field("#{method}_time_minute", input_html_options.merge({ :class => "just-datetime-picker-field just-datetime-picker-time just-datetime-picker-time-minute", :value => minute_value, :maxlength => 2, :size => 2 })) <<
+          " " <<
+          builder.select("#{method}_time_meridian", ["AM", "PM"], input_html_options.merge({ :class => 'just-datetime-picker-field just-datetime-picker-time just-datetime-picker-time-meridian'}, :value => builder.object.send("#{method}_time_meridian"))
         end
       end
     end
